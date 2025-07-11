10fca1fab090d43140643199db880328c16df562
joanreia16 <andreanredy6@gmail.com>
2025-07-11 13:03:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import sqlite3
 from datetime import datetime, timedelta
 from io import BytesIO
 import matplotlib.pyplot as plt
-st.cache_data.clear()
 
 # ---------- Konfigurasi ----------
 DB_FILE = 'keuangan.db'
